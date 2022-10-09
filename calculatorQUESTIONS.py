27e1dfac04d415a69f0e71664960d178d29c2cb4
Saad-A-Rahman15 <saad.a.rahman15@gmail.com>
2022-10-09 20:20:45 +0100

    This is my calculator from Python.

--- calculatorQUESTIONS.py
+++ calculatorQUESTIONS.py
@@ -1,16 +1,22 @@
-speech1 = input("Enter the first person's name:\n") #(Enter any name you want]
-question1 = input('How many years is he/she?\n') #(You can pick a boy or a girl]
-speech2 = input("Enter the second person's name:\n")
-question2 = input("How many years old is he/she?\n")
-speech3 = input("Enter the third person's name:\n")
-question3 = input("How many years old is he/she\n")
-if(question1 > (question2) and (question3)):
-    print('The oldest person is ' + (speech1))
-elif(question2 > (question1) and (question3)):
-    print('The oldest person is ' + (speech2))
-elif(question3 > (question1) and (question2)):
-    print('The oldest person is ' + (speech3))
-elif(question1 == (question3) and (question2)):
-    print('They are all the same age.')
+print('Start the questions.' )
+
+operations = input('+   -   *   /\n')
+number1 = input('Enter the first number.\n')
+number2 = input('Enter the second number.\n')
+if(operations == '+'):
+    print('The answer is:')
+    print((int(number1) + int(number2)))
+elif(operations == '-'):
+    print('The answer is:')
+    print((int(number1) - int(number2)))
+elif(operations == '*'):
+    print('The answer is:')
+    print((int(number1) * int(number2)))
+elif(operations == '/'):
+    if(number2 == '0'):
+        print('That is not allowed.')
+    else:
+        print('The answer is:')
+        print((int(number1) / int(number2)))
 else:
-    print('That cannot be done in the game.')
+    print("Sorry, you can't do that!")
